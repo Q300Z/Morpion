19ac33ad7eb324ca18859fd6f32e7e0902695d38
Q300Z <Q300Zhomas@Gmail.com>
2023-01-26 16:02:42 +0100

    Optimisation de la méthode ColorGrid

--- Program.cs
+++ Program.cs
@@ -23,21 +23,30 @@
             Console.WriteLine("║         ║         ║         ║");
             Console.WriteLine("╚═════════╩═════════╩═════════╝");
 
-            int x = 0;
+            int x = 2;
             int y = 1;
 
+
+
             //ColorGrid([1,11,21],[3,7,11])
-            ColorGrid(1+(10*x), 3+(y*4));//Le premier chiffre définit la position par défaut, le chiffre multiplié définit l'empattement de chaque cellule, la variable définit la cellule qu'on veut colorer
+            ColorGrid(x,y);
 
             Console.SetCursorPosition(1, 25);
         }
         static void ColorGrid(int x, int y) //Fait carré vert de 3 de haut pour 9 de large
         {
-            Console.BackgroundColor = ConsoleColor.Green;
+            /*Le premier chiffre définit la position par défaut, 
+             * le chiffre multiplié définit l'empattement de chaque cellule, 
+             * la variable définit la cellule qu'on veut colorer
+             */
+            x = 1 + (10 * x);
+            y = 3 + (4 * y);
+
+            Console.BackgroundColor = ConsoleColor.Green;//Affiche la couleur vert
             for (int h = 0; h < 3; h++)
             {
                 Console.SetCursorPosition(x, y + h);
-                Console.Write("         ");
+                Console.Write(new String(' ', 9));//Répéte un caractére x fois
 
                 /*for (int l = 0; l < 9; l++)
                 {
